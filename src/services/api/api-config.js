09d0142a6817e75325ad87d30176d9eeb704bb60
Mark <mark@tide.mx>
2020-04-04 11:16:35 -0600

    Added app version comparision with the one reported by the server Added custom service worker for reloading and push notifications

--- src/services/api/api-config.js
+++ src/services/api/api-config.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import semver from 'semver-lite';
+import {version} from '../../pckg';
 
 const config ={
     host:'http://localhost:8000',
     commonPath:'api',
     credentials:'include',
     saveTokenToLocalStorage: true,
-    getDataFromResponse:(r)=>r.data,
+    getDataFromResponse: handleResponse,
     getMetaDataFromResponse:(r)=>r.meta,
     endpoints:[
         'me',
@@ -26,8 +27,39 @@ const config ={
     ],
 };
 
-if(process.env.NODE_ENV === 'development')
-    config.host='http://localhost:8000';
+
+function handleResponse(response, headers){
+
+    try {
+        let server = headers.get("X-App-Version");
+        let force = !!server && server[server.length - 1] === 'f';
+        if (force) server = server.slice(0, server.length - 1);
+
+        if (semver.validate(version) && semver.validate(version) && semver.gt(server, version)) {
+            if(!this.newerVersion)
+                console.log("Newer version detected " + server);
+            if (!this.newerVersion && window.swRegistration) {
+                window.swRegistration.update()
+                    .then(() => {
+                        //This will broadcast the refresh message to all active tabs of the app through the sw
+                        if (force)
+                            window.navigator.serviceWorker.controller.postMessage('force_refresh');
+                    });
+            }
+            this.newerVersion = version;
+            if (force && !window.swRegistration)
+                window.location.reload();
+        }
+    }
+    catch(e){
+        console.error('Error reading versions: '+e);
+    }
+
+    return response.data;
+}
+
+if(process.env.REACT_APP_BUILD === 'dev')
+    config.host=localStorage.host||'http://localhost:8000';
 
 export default config;
 
